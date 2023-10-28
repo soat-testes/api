14e2df418b62db696c2518b793c88e12b32bbcc7
Pedro Haramoto <pedroharamoto@gmail.com>
2023-10-28 19:29:28 -0300

    add ping controller

---
+++ src/main/java/com/soat/api/controller/PingController.java
@@ -0,0 +1,16 @@
+package com.soat.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/soat/api")
+public class PingController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<?> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
---
+++ src/test/java/com/soat/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soat.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
